734895ca1e04bc40617f5bba2a91f4e6ea1d0d33
Andrew Edvalson <andy.edvalson@infotechfl.com>
2018-04-26 20:17:26 +0000

    Fixing logging

--- upload_script.sh
+++ upload_script.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-echo "`date -u` $1 has been uploaded" >> /tmp/ftp.log
-echo "`date -u` `aws s3 cp $1 s3://daypacer-incoming-recordings` >> /tmp/ftp.log
+echo "`date -u`" >> /tmp/ftp.log
+echo " $1 has been uploaded" >> /tmp/ftp.log
+echo "`date -u` `aws s3 cp $1 s3://daypacer-incoming-recordings`" >> /tmp/ftp.log
+echo "" >> /tmp/ftp.log
