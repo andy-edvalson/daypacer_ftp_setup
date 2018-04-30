e9b7199c0bfafc19505836218ae147ccead5a66d
Andrew Edvalson <andy.edvalson@infotechfl.com>
2018-04-30 03:33:24 +0000

    Adding lftp relay stuff (WIP)

--- upload_script.sh
+++ upload_script.sh
@@ -2,4 +2,7 @@
 echo "`date -u`" >> /tmp/ftp.log
 echo " $1 has been uploaded" >> /tmp/ftp.log
 echo "`date -u` `aws s3 cp $1 s3://daypacer-incoming-recordings`" >> /tmp/ftp.log
+#lftp -c "open -u recordings,daypacer localhost; put -O / README.md"
 echo "" >> /tmp/ftp.log
+
+
