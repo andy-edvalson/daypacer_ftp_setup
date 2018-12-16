40a017a639ffa83ed9e5ed06a85c89afc791c70a
Andy Edvalson <andy.edvalson@gmail.com>
2018-12-16 13:13:55 -0700

    Fixing CNS regex for more forging datetimes

--- hourly_job.sh
+++ hourly_job.sh
@@ -9,7 +9,7 @@ declare OUT_PATH="/data/staging"
 declare IN_PATH="/data/recordings"
 declare MINUTE_DELAY=60
 
-declare CNS_REGEX="([a-z\/]+)CNS_([0-9-]+)_([0-9:]+)_([0-9]+).wav"  # 1: Path  2: Date  3: Time  4: Phone
+declare CNS_REGEX="([a-z\/]+)CNS_([0-9-]+)_([0-9:\sAMP]+)_([0-9]+).wav"  # 1: Path  2: Date  3: Time  4: Phone
 declare CNS_S3_BUCKET="cns-recordings"
 
 if [ ! -d $OUT_PATH ]
